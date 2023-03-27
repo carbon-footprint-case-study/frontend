c025b8a2ddc25eb3068e6e2811219f2a7db33a2f
Abhay Madhukar Ahire <abhayahire2002@gmail.com>
2023-03-27 12:24:08 +0530

    fixed the bug in footprint component

--- src/components/FootPrint.js
+++ src/components/FootPrint.js
@@ -1,56 +1,117 @@
-import React from 'react'
+// import React from 'react'
+// import BarGraph from './BarGraph'
+// import { useState , useEffect} from 'react'
+
+// function FootPrint(props) {
+
+//   const homeData = props.homeData
+//   const travelData = props.travelData
+//   const homeLabels = props.homeLabels
+//   const travelLabels = props.travelLabels
+//   const units = ['Home' , 'Travel' , 'Total']
+//   // const [unit, setUnit] = useState(units[0]);
+//   // const [data ,  setData] = useState(homeData)
+//   // const [labels , setLabels] = useState(homeLabels)
+//   // const [totalData , setTotalData] = useState([0 , 0])
+
+//   const [totalData, setTotalData] = useState([
+//     homeData.reduce((a, b) => a + b, 0),
+//     travelData.reduce((a, b) => a + b, 0),
+//   ])
+//   const [unit, setUnit] = useState(units[0])
+//   const [data, setData] = useState(homeData)
+//   const [labels, setLabels] = useState(homeLabels)
+
+  
+
+
+//   const unitHandler = (ele) => {
+//     let curr = ele.target.value
+//     if(curr == 'Home'){
+//       setData(homeData)
+//       setLabels(homeLabels)
+//     }
+//     else if(curr == 'Travel'){
+//       setData(travelData)
+//       setLabels(travelLabels)
+//     }
+//     else{
+   
+//       setData(totalData)
+//       setLabels(['Home' , 'Travel'])
+//     }
+//     setUnit(ele.target.value)
+//   }
+//   return (
+//     <>
+//     <select
+//     className="dropdown"
+//     onChange={unitHandler}
+//   >
+//     {units.map((currElement) => {
+//       return <option key={currElement}>{currElement}</option>;
+//     })}
+//   </select>
+//     <BarGraph data = {data} labels = {labels} key = {String(data)}/>
+//     </>
+//   )
+// }
+
+// export default FootPrint
+
+import React, { useState, useEffect } from 'react'
 import BarGraph from './BarGraph'
-import { useState , useEffect} from 'react'
 
 function FootPrint(props) {
-
-  const homeData = props.homeData
-  const travelData = props.travelData
+  const homeData = JSON.parse(localStorage.getItem('homeData')) || props.homeData
+  const travelData = JSON.parse(localStorage.getItem('travelData')) || props.travelData
   const homeLabels = props.homeLabels
   const travelLabels = props.travelLabels
-  const units = ['Home' , 'Travel' , 'Total']
-  const [unit, setUnit] = useState(units[0]);
-  const [data ,  setData] = useState(homeData)
-  const [labels , setLabels] = useState(homeLabels)
-  const [totalData , setTotalData] = useState([0 , 0])
+  const units = ['Home', 'Travel', 'Total']
 
+  const [totalData, setTotalData] = useState([
+    homeData.reduce((a, b) => a + b, 0),
+    travelData.reduce((a, b) => a + b, 0),
+  ])
+  const [unit, setUnit] = useState(units[0])
+  const [data, setData] = useState(homeData)
+  const [labels, setLabels] = useState(homeLabels)
 
   useEffect(() => {
-    const homeSum = homeData.reduce((acc, curr) => acc + curr, 0);
-    const travelSum = travelData.reduce((acc, curr) => acc + curr, 0);
-    setTotalData([homeSum, travelSum]);
-  }, [homeData, travelData]);
-
+    localStorage.setItem('homeData', JSON.stringify(homeData))
+    localStorage.setItem('travelData', JSON.stringify(travelData))
+  }, [homeData, travelData])
 
   const unitHandler = (ele) => {
     let curr = ele.target.value
-    if(curr == 'Home'){
+    if (curr === 'Home') {
       setData(homeData)
       setLabels(homeLabels)
-    }
-    else if(curr == 'Travel'){
+    } else if (curr === 'Travel') {
       setData(travelData)
       setLabels(travelLabels)
-    }
-    else{
+    } else {
       setData(totalData)
-      setLabels(['Home' , 'Travel'])
+      setLabels(['Home', 'Travel'])
     }
-    setUnit(ele.target.value)
+    setUnit(curr)
   }
+
   return (
     <>
-    <select
-    className="dropdown"
-    onChange={unitHandler}
-  >
-    {units.map((currElement) => {
-      return <option key={currElement}>{currElement}</option>;
-    })}
-  </select>
-    <BarGraph data = {data} labels = {labels} key = {String(data)}/>
+      <select className="dropdown" onChange={unitHandler}>
+        {units.map((currElement) => {
+          return (
+            <option key={currElement} value={currElement}>
+              {currElement}
+            </option>
+          )
+        })}
+      </select>
+      <BarGraph data={data} labels={labels} key={String(data)} />
     </>
   )
 }
 
 export default FootPrint
+
